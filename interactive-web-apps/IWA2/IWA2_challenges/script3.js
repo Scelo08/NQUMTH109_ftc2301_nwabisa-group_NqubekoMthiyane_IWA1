224f065120ffdd0bb4d48dab4745ec099a318c1a
Nqubeko <yandibongi001@gmail.com>
2023-03-28 14:22:03 +0200

    Update script3.js

--- interactive-web-apps/IWA2/IWA2_challenges/script3.js
+++ interactive-web-apps/IWA2/IWA2_challenges/script3.js
@@ -9,4 +9,4 @@ console.warn("Security scan starting");
 
 //It is important to let user know when they can close the page
 
-cnosole.log("Please wait for scan to complete before closing the browser.");
+cnosole.info("Please wait for scan to complete before closing the browser.");
