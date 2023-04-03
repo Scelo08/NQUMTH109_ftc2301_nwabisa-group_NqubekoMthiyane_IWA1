08bf8234ac408b93693ea9486d9ccd757e6fa94e
Nqubeko <yandibongi001@gmail.com>
2023-04-03 15:29:38 +0200

    JS changes

--- interactive-web-apps/IWA_5.2: challenge 1/JS code/script.js
+++ interactive-web-apps/IWA_5.2: challenge 1/JS code/script.js
@@ -2,32 +2,34 @@ const FREE_WARNING = 'Free shipping only applies to single customer orders'
 const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
 const NONE_SELECTED = '0'
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+if (location === RSA) { shipping === 400 && currency === 'R' }
 
-if (location = NAM){
+if (location === NAM){
     shipping === 600 && currency === '$'
 } else{
     shipping === 800
 }
 
-shoes = 300 * 1
-toys = 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries = 35 * 2
-pens = 5 * 'NONE_SELECTED' 
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * 'NONE_SELECTED'
+let batteries = 35 * 2
+let pens = 5 * 'NONE_SELECTED' 
 
-shipping = null
+let shipping = null
 currency = $
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
+if (shoes && batteries && pens && shirts > 1000) {
 	if (location = NAM && customers < 2) {
 			if (location = RSA)
 		    shipping = 0 || calcShipping
 		}
 	}
-}
 
-if (shipping = 0) && (customers !== 1) { console.log(WARNING) }
+
+if (shipping === 0 && customers !== 1) { 
+    console.log(WARNING) 
+}
 
 location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
