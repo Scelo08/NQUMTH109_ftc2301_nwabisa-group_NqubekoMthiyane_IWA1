5e7ac73b9ba94fc98d343e86bb7ec79ede2d795f
Nqubeko <yandibongi001@gmail.com>
2023-05-02 10:47:35 +0200

    first commit

---
+++ interactive-web-apps/IWA15/script.js
@@ -0,0 +1,49 @@
+// scripts.js
+
+const data = {
+	lists: [
+		['first', [15, 11, 13, 7, 5]],
+		['second', [2, 6, 8, 4, 14, 12, 10]],
+		['third', [9, 3, 1]],
+	]
+}
+
+// Only edit below
+
+const [first, second, third] = data.lists.map(item => item[1]);
+
+const result = []
+
+const extractBiggest = () => {
+	if (first[first.length - 1] > second[second.length - 1]) {
+		return first
+	}
+
+	if (third.length > 0 && third[third.length - 1] > first[first.length - 1]) {
+		return third
+	}
+	
+	return second
+}
+
+// Only edit above
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+console.log(result)
